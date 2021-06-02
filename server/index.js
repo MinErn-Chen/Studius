db4b5b77d8ab9eaae50a78d5a0078fec4116af77
Peter <wjdgustjr0554@gmail.com>
2021-06-02 22:57:07 +0900

    Installed morgan middleware to log HTTP requests

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const morgan = require("morgan");
 
 // default port
 const port = 3000;
@@ -8,6 +9,7 @@ const port = 3000;
 // middleware
 app.use(express.json());
 app.use(cors());
+app.use(morgan("dev"));
 
 // register and login routes
 app.use("/auth", require("./routes/jwtAuth"));
