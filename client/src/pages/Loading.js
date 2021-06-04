b6498a6464282bb194fb3ed0c96abb4e1cc208b4
Peter <wjdgustjr0554@gmail.com>
2021-06-04 17:18:08 +0900

    Created loading screen to handle asynchrony of authentication

---
+++ client/src/pages/Loading.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  loadingText: {
+    paddingTop: theme.spacing(2),
+  },
+}));
+
+export default function CircularIndeterminate() {
+  const classes = useStyles();
+
+  return (
+    <>
+      <Grid
+        container
+        className={classes.root}
+        direction="column"
+        justify="center"
+        alignItems="center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={12}>
+          <CircularProgress />
+        </Grid>
+        <Grid item xs={12} className={classes.loadingText}>
+          <Typography variant="h5">Loading...</Typography>
+        </Grid>
+      </Grid>
+    </>
+  );
+}
