9d8573a3631afc990a0aa5a9a98501ef0c9f6829
Peter <wjdgustjr0554@gmail.com>
2021-06-08 21:22:16 +0900

    Removed redundant divider

--- client/src/components/Profile/Contents/AccountInformation.js
+++ client/src/components/Profile/Contents/AccountInformation.js
@@ -63,7 +63,6 @@ const AccountInformation = ({ accountInformation, handleDialogueOpen }) => {
             </>
           )
         )}
-        <Divider component="li" />
       </List>
     </>
   );
