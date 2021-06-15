42ddba2cfdece338c68e33c7efc850274af157be
Peter <wjdgustjr0554@gmail.com>
2021-06-15 15:18:43 +0900

    Fully implemented tutor credentials CRUD features

--- server/routes/fileUpload.js
+++ server/routes/fileUpload.js
@@ -2,13 +2,14 @@ const router = require("express").Router();
 const authorisation = require("../middleware/authorisation");
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
 const pool = require("../db");
 
 // initialise file storage destination
 const tutorCredentialsStorage = multer.diskStorage({
-  // files to be stored in files/tutor_credentials
+  // files to be stored in files/credentials
   destination: (req, file, cb) => {
-    cb(null, "files/tutor_credentials");
+    cb(null, "files/credentials");
   },
   // files to be stored as pdf with name of user id
   filename: (req, file, cb) => {
@@ -33,8 +34,8 @@ const tutorCredentialsUpload = multer({
 
 // upload tutor credentials
 router.put(
-  "/tutor_credentials",
-  [authorisation, tutorCredentialsUpload.single("tutor_credential")],
+  "/credentials",
+  [authorisation, tutorCredentialsUpload.single("credential")],
   async (req, res) => {
     try {
       // check if file type is correct
@@ -42,44 +43,45 @@ router.put(
         return res.json(req.fileValidationError);
       }
 
-      // destructure the req.file (mimtype, size, path)
+      // destructure the req.file (mimetype, size, path)
       const { mimetype, size, path } = req.file;
 
       // determine whether file already exists and insert/update accordingly
       const filePresence = await pool.query(
-        "SELECT * FROM tutor_credentials WHERE id = $1",
+        "SELECT * FROM credentials WHERE id = $1",
         [req.user.id]
       );
       if (filePresence.rows.length === 0) {
         await pool.query(
-          "INSERT INTO tutor_credentials VALUES ($1, $2, $3, $4, $5)",
-          [req.user.id, req.user, path, mimetype, size]
+          "INSERT INTO credentials VALUES ($1, $2, $3, $4, $5)",
+          [req.user.id, req.user.id, path, mimetype, size]
         );
       } else {
         await pool.query(
-          "UPDATE tutor_credentials SET filename = $1, filepath = $2, mimetype = $3, size = $4 WHERE id = $5",
-          [req.user.id, path, mimetype, size, req.user]
+          "UPDATE credentials SET filename = $1, filepath = $2, mimetype = $3, size = $4 WHERE id = $5",
+          [req.user.id, path, mimetype, size, req.user.id]
         );
       }
 
       res.json("Upload successful!");
     } catch (error) {
+      console.error(error);
       res.status(500).json("Server error");
     }
   }
 );
 
 // get tutor credentials
-router.get("/tutor_credentials", authorisation, async (req, res) => {
+router.get("/credentials", authorisation, async (req, res) => {
   try {
     // retrieve tutor credentials tied to user id and handle error accordingly
     const fileRetrieve = await pool.query(
-      "SELECT * FROM tutor_credentials WHERE id = $1",
+      "SELECT * FROM credentials WHERE id = $1",
       [req.user.id]
     );
 
     if (fileRetrieve.rows.length === 0) {
-      return res.status(404).json("No file found");
+      return res.status(404).json("No file to retrieve");
     }
 
     // return the file stored at the location specified in the database
@@ -89,6 +91,37 @@ router.get("/tutor_credentials", authorisation, async (req, res) => {
     res.type(fileRetrieve.rows[0].mimetype).sendFile(fullfilepath);
   } catch (error) {
     res.status(500).json("Server error");
+  }
+});
+
+// remove tutor credentials
+router.delete("/credentials", authorisation, async (req, res) => {
+  try {
+    if (req.user.type === "Student") {
+      return res.status(403).json("Not authorised");
+    }
+
+    const fileDelete = await pool.query(
+      "DELETE FROM credentials WHERE id = $1 RETURNING *",
+      [req.user.id]
+    );
+
+    if (fileDelete.rows.length === 0) {
+      return res.status(404).json("No file to delete");
+    }
+
+    fs.unlink(`./files/credentials/${req.user.id}.pdf`, (error) => {
+      if (error) {
+        console.error(`Failed to delete credential: ${error}`);
+        return res.status(400).json(`Failed to delete credential: ${error}`);
+      } else {
+        console.log("Successfully deleted credential!");
+        return res.json("Successfully deleted credential!");
+      }
+    });
+  } catch (error) {
+    console.error(`Failed to delete credential: ${error}`);
+    return res.status(400).json(`Failed to delete credential: ${error}`);
   }
 });
 
