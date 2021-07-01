97e20cf43e68ac5324849f8c63ddbd5471ad180e
Peter <wjdgustjr0554@gmail.com>
2021-07-01 16:36:35 +0900

    Implemented rudimentary contractual interface

--- client/src/index/App/Main/Marketplace/ProfileView.js
+++ client/src/index/App/Main/Marketplace/ProfileView.js
@@ -1,5 +1,15 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
@@ -42,6 +52,9 @@ const useStyles = makeStyles((theme) => ({
   fileUpload: {
     display: "none",
   },
+  contractText: {
+    marginTop: theme.spacing(2),
+  },
 }));
 
 const ProfileView = ({
@@ -51,6 +64,7 @@ const ProfileView = ({
   match,
   credentialsURL,
   setCredentialsURL,
+  contractSubjects,
 }) => {
   const classes = useStyles();
 
@@ -63,79 +77,159 @@ const ProfileView = ({
     setCredentialsURL("");
   };
 
-  return (
-    <main className={classes.layout}>
-      <Paper className={classes.paper}>
-        <Typography className={classes.title} variant="h4" align="center">
-          {`${profile.firstname} ${profile.lastname} `}
-        </Typography>
-        <Grid container spacing={2} justify="center" direction="column">
-          <Grid item xs={12}>
-            <Typography variant="h6">Subjects</Typography>
-            <Typography>
-              {profile.subjects
+  const [contractOpen, setContractOpen] = useState(false);
+
+  const handleContractOpen = () => {
+    setContractOpen(true);
+  };
+
+  const handleContractClose = () => {
+    setContractOpen(false);
+  };
+
+  const [contractSubject, setContractSubject] = useState("");
+
+  const handleContractConfirm = async (event) => {
+    event.preventDefault();
+    console.log(contractSubject);
+  };
+
+  const handleContractChange = (event) => {
+    setContractSubject(event.target.value);
+  };
+
+  const Contract = () => {
+    return (
+      <Dialog open={contractOpen} maxWidth="xs" fullWidth>
+        <DialogTitle id="contract-title">Test</DialogTitle>
+        <DialogContent>
+          <FormControl variant="outlined" fullWidth required>
+            <InputLabel id="subject">Subject</InputLabel>
+            <Select
+              labelId="subject"
+              id="subject"
+              value={contractSubject}
+              name="subject"
+              onChange={handleContractChange}
+              label="Subject"
+              MenuProps={{
+                anchorOrigin: {
+                  vertical: "top",
+                  horizontal: "left",
+                },
+                transformOrigin: {
+                  vertical: "top",
+                  horizontal: "left",
+                },
+                getContentAnchorEl: null,
+              }}
+            >
+              {contractSubjects
                 .map((subject) =>
                   Array.isArray(subject) ? subject.join(" ") : subject
                 )
-                .join(", ")}
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Typography variant="h6">Rate</Typography>
-            <Typography>$ {profile.rate} / hr</Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Typography variant="h6">Available times</Typography>
-            <Typography>
-              {`${moment(profile.times[0], "HH:mm").format(
-                "hh:mm A"
-              )} — ${moment(profile.times[1], "HH:mm").format("hh:mm A")}`}
-            </Typography>
-          </Grid>
-          {userInformation.type === "Student" ? (
-            <Grid item xs={12}>
-              <Typography variant="h6">Education</Typography>
-              <Typography>{profile.education}</Typography>
-            </Grid>
-          ) : null}
-          <Grid item xs={12}>
-            <Typography variant="h6">Description</Typography>
-            <Typography>{profile.description}</Typography>
-          </Grid>
-          <Grid item xs={12}></Grid>
-        </Grid>
-        <div className={classes.buttons}>
-          {userInformation.type === "Student" ? (
-            <Button
-              variant="outlined"
-              color="primary"
-              className={classes.button}
-              onClick={handleViewCredentials}
-              disabled={!Boolean(credentialsURL)}
-            >
-              {Boolean(credentialsURL) ? "View credentials" : "No credentials"}
-            </Button>
-          ) : null}
-          <Button
-            color="secondary"
-            className={classes.button}
-            onClick={handleProfileClose}
-            component={Link}
-            to={match.url}
+                .map((subject) => (
+                  <MenuItem value={subject}>{subject}</MenuItem>
+                ))}
+            </Select>
+          </FormControl>
+          <DialogContentText
+            className={classes.contractText}
+            id="contract-description"
           >
+            You will engage in tuition upon confirmation.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleContractClose} color="primary">
             Cancel
           </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            className={classes.button}
-            onClick={() => console.log(2)}
-          >
+          <Button onClick={handleContractConfirm} color="primary" autoFocus>
             Confirm
           </Button>
-        </div>
-      </Paper>
-    </main>
+        </DialogActions>
+      </Dialog>
+    );
+  };
+
+  return (
+    <>
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Typography className={classes.title} variant="h4" align="center">
+            {`${profile.firstname} ${profile.lastname} `}
+          </Typography>
+          <Grid container spacing={2} justify="center" direction="column">
+            <Grid item xs={12}>
+              <Typography variant="h6">Subjects</Typography>
+              <Typography>
+                {profile.subjects
+                  .map((subject) =>
+                    Array.isArray(subject) ? subject.join(" ") : subject
+                  )
+                  .join(", ")}
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h6">Rate</Typography>
+              <Typography>$ {profile.rate} / hr</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h6">Available times</Typography>
+              <Typography>
+                {`${moment(profile.times[0], "HH:mm").format(
+                  "hh:mm A"
+                )} — ${moment(profile.times[1], "HH:mm").format("hh:mm A")}`}
+              </Typography>
+            </Grid>
+            {userInformation.type === "Student" ? (
+              <Grid item xs={12}>
+                <Typography variant="h6">Education</Typography>
+                <Typography>{profile.education}</Typography>
+              </Grid>
+            ) : null}
+            <Grid item xs={12}>
+              <Typography variant="h6">Description</Typography>
+              <Typography>{profile.description}</Typography>
+            </Grid>
+            <Grid item xs={12}></Grid>
+          </Grid>
+          <div className={classes.buttons}>
+            {userInformation.type === "Student" ? (
+              <Button
+                variant="outlined"
+                color="primary"
+                className={classes.button}
+                onClick={handleViewCredentials}
+                disabled={!Boolean(credentialsURL)}
+              >
+                {Boolean(credentialsURL)
+                  ? "View credentials"
+                  : "No credentials"}
+              </Button>
+            ) : null}
+            <Button
+              color="secondary"
+              className={classes.button}
+              onClick={handleProfileClose}
+              component={Link}
+              to={match.url}
+            >
+              Back
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.button}
+              onClick={handleContractOpen}
+            >
+              Send contract
+            </Button>
+          </div>
+        </Paper>
+      </main>
+      <Contract />
+    </>
   );
 };
 
