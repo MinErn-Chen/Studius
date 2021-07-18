6f66807374db17ef48174a6e41b9f90889f178dc
Peter <wjdgustjr0554@gmail.com>
2021-07-19 00:18:44 +0900

    Resolved SSL certificate issue

--- server/db.js
+++ server/db.js
@@ -1,11 +1,20 @@
 const Pool = require("pg").Pool;
 
+// const pool = new Pool({
+//   user: "postgres",
+//   password: "raspberry",
+//   host: "localhost",
+//   port: 5432,
+//   database: "studius",
+// });
+
 const pool = new Pool({
-  user: "postgres",
-  password: "raspberry",
-  host: "localhost",
+  user: "kdniquunuukruq",
+  password: "7886f6bc28b7b2420d361c7b34454e339e6e9a90cc14b29ad45f085a950c5aa8",
+  host: "ec2-3-233-7-12.compute-1.amazonaws.com",
   port: 5432,
-  database: "studius",
+  database: "d7dioab7opn3ti",
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
