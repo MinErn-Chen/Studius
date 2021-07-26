7b237fe5b163dafc2e5813920816b2d8ad56a5f1
Peter <wjdgustjr0554@gmail.com>
2021-07-26 22:15:12 +0900

    Obscured information

--- server/db.js
+++ server/db.js
@@ -8,13 +8,4 @@ const pool = new Pool({
   database: "studius",
 });
 
-// const pool = new Pool({
-//   user: "kdniquunuukruq",
-//   password: "7886f6bc28b7b2420d361c7b34454e339e6e9a90cc14b29ad45f085a950c5aa8",
-//   host: "ec2-3-233-7-12.compute-1.amazonaws.com",
-//   port: 5432,
-//   database: "d7dioab7opn3ti",
-//   ssl: { rejectUnauthorized: false },
-// });
-
 module.exports = pool;
